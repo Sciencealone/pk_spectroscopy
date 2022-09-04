44a8c0050f023a9513cb47c286b4c97f93e72ff4
Mikhail Markovsky <m.markovsky@gmail.com>
2022-09-04 11:30:48 +0300

    Added a titration curve display.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from bokeh.plotting import figure
 from pk_spectrum import pKSpectrum
 
 st.title('pK spectroscopy app')
@@ -27,9 +28,31 @@ if uploaded_file is not None:
     for label, item in [
         ('Sample name', pks.sample_name),
         ('Comment', pks.comment),
-        ('Date', pks.date),
-        ('Time', pks.time),
+        ('Timestamp', pks.timestamp),
         ('Sample volume', pks.sample_volume),
-        ('Alkaline concentration', pks.alkaline_concentration)
+        ('Alkaline concentration', pks.alkaline_concentration),
+        ('Data points', len(pks.alkaline_volumes))
     ]:
         st.sidebar.write(f'{label}: {item}')
+
+    # Display titration curve
+    with st.expander('Titration curve'):
+
+        # Check we have any data
+        if len(pks.alkaline_volumes) > 0:
+            title = 'Titration curve'
+            if pks.sample_name:
+                title = f'{title} ({pks.sample_name})'
+
+            p = figure(
+                title=title,
+                x_axis_label='Titrant volume',
+                y_axis_label='pH')
+            p.line(pks.alkaline_volumes, pks.ph_values, line_width=2)
+            st.bokeh_chart(p, use_container_width=True)
+        else:
+            st.write('Not enough data!')
+
+
+else:
+    st.write('⬅ Waiting for a data file in the sidebar.')
