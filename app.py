c90a05d20b2ffe3932ff22734acdc760e322f228
Mikhail Markovsky <m.markovsky@gmail.com>
2022-09-10 14:05:03 +0300

    Added main calculations.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 from bokeh.plotting import figure
 from pk_spectrum import pKSpectrum
@@ -35,8 +36,8 @@ if uploaded_file is not None:
     ]:
         st.sidebar.write(f'{label}: {item}')
 
-    # Display titration curve
-    with st.expander('Titration curve'):
+    # Display titration curve expander
+    with st.expander('Source data'):
 
         # Check we have any data
         if len(pks.alkaline_volumes) > 0:
@@ -57,6 +58,26 @@ if uploaded_file is not None:
         else:
             st.write('Not enough data!')
 
+    # Gather main control variables expander
+    with st.expander('Parameters', expanded=True):
+        pk_start = st.number_input('Start pK:', value=0., min_value=0., max_value=10.)
+        pk_end = st.number_input('End pK:', value=10., min_value=0., max_value=10.)
+        d_pk = st.number_input('pK step (0.05-0.1 recommended):', value=.05, min_value=0.01, max_value=1.)
+
+    # Check values
+    if pk_start > pk_end:
+        pk_start, pk_end = pk_end, pk_start
+    if d_pk > pk_end - pk_start:
+        d_pk = 0.05
+
+    # Get and display results
+    peaks, error = pks.make_calculation(pk_start, pk_end, d_pk)
+    result_df = pd.DataFrame(peaks)
+    if not result_df.empty:
+        with st.expander('Results', expanded=True):
+            st.write('Peaks:')
+            st.write(result_df[['concentration', 'mean', 'interval']])
+            st.write(f'Error: {error:.5}')
 
 else:
     st.write('⬅ Waiting for a data file in the sidebar.')
--- pk_spectrum.py
+++ pk_spectrum.py
@@ -1,7 +1,10 @@
+import numpy as np
+from scipy.optimize import nnls
 from openpyxl import load_workbook
 
 Kw = 1e-14
-
+LOG10 = np.log(10)
+TOLERANCE = 1e-6
 
 class pKSpectrum():
     def __init__(self, source_file):
@@ -16,6 +19,7 @@ class pKSpectrum():
         self.ph_values = []
         self.alpha_values = []
         self.valid_points = None
+        self.acid_peaks = []
 
         self._load_data()
 
@@ -67,6 +71,75 @@ class pKSpectrum():
                 self.valid_points = i + 1
             else:
                 break
+
+    def make_calculation(self, pk_start, pk_end, d_pk):
+
+        # Calculate constant step
+        pk_step = round((pk_end - pk_start) / d_pk) + 1
+
+        # Fill right part
+        right = np.zeros((self.valid_points, pk_step))
+        for i in range(self.valid_points):
+            for j in range(pk_step):
+                right[i, j] = d_pk / (1 + np.exp(LOG10 * pk_start + d_pk * j - self.ph_values[i]))
+
+        # Solve equation
+        constants, residual = nnls(right, np.array(self.alpha_values))
+
+        # Normalization
+        constants *= d_pk
+
+        # Truncate border artefacts
+        if constants[0] > TOLERANCE > constants[1]:
+            constants[0] = 0
+        if constants[-1] > TOLERANCE > constants[-2]:
+            constants[-1] = 0
+
+        sum_constants = constants.sum()
+        max_constant = constants.max(initial=0)
+        threshold = max_constant / 100
+        constants_relative = constants / sum_constants
+
+        # Peak calculation sequence
+        i = 0
+        while i < pk_step:
+            if constants[i] > threshold:
+                self.acid_peaks.append({'point_count': 0, 'concentration': 0, 'first_point': i})
+                while i < pk_step and constants[i] > threshold:
+                    self.acid_peaks[-1]['point_count'] += 1
+                    self.acid_peaks[-1]['concentration'] += constants[i]
+                    i += 1
+            else:
+                i += 1
+
+        # Peaks exact position and height calculation
+        if len(self.acid_peaks) > 0:
+            for i in range(len(self.acid_peaks)):
+                t1 = 0
+                t2 = 0
+                peak = self.acid_peaks[i]
+                for j in range(peak['point_count']):
+                    t1 += constants_relative[peak['first_point'] + j] * \
+                        (pk_start + d_pk * (peak['first_point'] + j))
+                    t2 += constants_relative[peak['first_point'] + j]
+                peak['mean'] = t1 / t2
+            for i in range(len(self.acid_peaks)):
+                peak = self.acid_peaks[i]
+                if peak['point_count'] > 0:
+                    t1 = 0
+                    t2 = 0
+                    for j in range(peak['point_count']):
+                        t1 += constants_relative[peak['first_point'] + j] * \
+                              (pk_start + d_pk * (peak['first_point'] + j) - peak['mean']) ** 2
+                        t2 += constants_relative[peak['first_point'] + j]
+                    peak['interval'] = 1.96 * np.sqrt(t1 / t2) / np.sqrt(peak['point_count'])
+                else:
+                    peak['interval'] = 0.
+
+        # Calculate error
+        error = np.sqrt(residual) / np.sqrt(pk_step - 1)
+
+        return self.acid_peaks, error
 
     @staticmethod
     def _check_number(a):
